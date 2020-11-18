848f91978c5769c8964582d91448ad51b3f16e71
aldrinburgos <burgos.aldrin@gmail.com>
2020-11-18 15:20:53 -0600

    html generator

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+/* const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
@@ -13,4 +13,15 @@ const printProfileData = profileDataArr => {
   profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs); */
+
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name,github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Checkout index.html to see the output!')
+});
