5a48be78e303cc87feffa76511a14fd1638f6c70
aldrinburgos <burgos.aldrin@gmail.com>
2020-11-21 06:32:05 -0600

    modify html to handle more data

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const inquirer = require('inquirer');
-/* const fs = require('fs');
+const fs = require('fs');
 const generatePage = require('./src/page-template');
 
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Checkout index.html to see the output!')
-});
- */
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -61,7 +53,7 @@ const promptUser = () => {
 
 
 const promptProject = portfolioData => {
-  //If there's no 'projects' array property, create on
+  //If there's no 'projects' array property, create one
   if(!portfolioData.projects) {
   portfolioData.projects = [];
   }
@@ -139,8 +131,61 @@ Add a New Project
   });
 };
 
+const mockData = 
+{
+  name: 'Lernantino',
+  github: 'lernantino',
+  confirmAbout: true,
+  about:
+    'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+  projects: [
+    {
+      name: 'Run Buddy',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+      languages: ['HTML', 'CSS'],
+      link: 'https://github.com/lernantino/run-buddy',
+      feature: true,
+      confirmAddProject: true
+    },
+    {
+      name: 'Taskinator',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+      languages: ['JavaScript', 'HTML', 'CSS'],
+      link: 'https://github.com/lernantino/taskinator',
+      feature: true,
+      confirmAddProject: true
+    },
+    {
+      name: 'Taskmaster Pro',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+      languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+      link: 'https://github.com/lernantino/taskmaster-pro',
+      feature: false,
+      confirmAddProject: true
+    },
+    {
+      name: 'Robot Gladiators',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+      languages: ['JavaScript'],
+      link: 'https://github.com/lernantino/robot-gladiators',
+      feature: false,
+      confirmAddProject: false
+    }
+  ]
+};
+
 promptUser()
-    .then(promptProject)
+ /*    .then(promptProject) */
     .then(portfolioData => {
-      console.log(projectAnswers);
+   const pageHTML = generatePage(mockData);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!')
+});
     });
